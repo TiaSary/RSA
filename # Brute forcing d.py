fb28966f3d191c245d08c48b7dffb47042ef34a8
TiaSary <145559326+TiaSary@users.noreply.github.com>
2024-04-18 07:29:15 +0200

    add functions

--- # Brute forcing d.py
+++ # Brute forcing d.py
@@ -15,11 +15,15 @@ def generate_rsa_keys(bit_length):
     
     return public_key, private_key
 
+
+
 def generate_prime_number(bit_length):
     while True:
         candidate = random.getrandbits(bit_length)
         if is_prime(candidate):
             return candidate
+
+
 
 def is_prime(n, k=5):
    
@@ -36,7 +40,7 @@ def is_prime(n, k=5):
     while d % 2 == 0:
         d //= 2
         r += 1
-    
+
     def miller_rabin_test(a):
         x = pow(a, d, n)
         if x == 1 or x == n - 1:
@@ -54,12 +58,16 @@ def is_prime(n, k=5):
     
     return True
 
+
+
 def choose_public_exponent(phi_n):
  
     e = random.randrange(2, phi_n)
     while math.gcd(e, phi_n) != 1:
         e = random.randrange(2, phi_n)
     return e
+
+
 
 def extended_euclidean_algorithm(a, b):
     
@@ -70,6 +78,8 @@ def extended_euclidean_algorithm(a, b):
     y = x1 - (a // b) * y1
     return gcd, x, y
 
+
+
 def modular_inverse(e, phi_n):
  
     gcd, x, _ = extended_euclidean_algorithm(e, phi_n)
@@ -78,7 +88,31 @@ def modular_inverse(e, phi_n):
     else:
         raise ValueError("No modular inverse exists")
 
-public_key, private_key = generate_rsa_keys(bit_length=1024)
+
+
+def encryption(message,public_key):
+    n, e = public_key
+    cipher = pow(message, e, n)
+    return cipher
+
+
+
+def decryption(cipher,private_key):
+    d, n = private_key
+    message = pow(cipher, d, n)
+    return message
+
+
+
+
+# Public and private key generation
+bit_length= int(input("Enter bit length: "))
+public_key, private_key = generate_rsa_keys(bit_length)
 print("Public Key (e, n):", public_key)
 print("Private Key (d, n):", private_key)
 
+
+
+
+
+# Brute forcing d
